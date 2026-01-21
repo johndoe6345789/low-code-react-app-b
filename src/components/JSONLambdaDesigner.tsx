06cca3cd9c498800b2e1c9ce6784015821daff13
johndoe6345789 <john.doe6345789@gmail.com>
2026-01-21 05:03:34 +0000

    feat: remove JSONLambdaDesigner TSX wrapper (already JSON component)

--- src/components/JSONLambdaDesigner.tsx
+++
@@ -1,9 +0,0 @@
-import { PageRenderer } from '@/lib/json-ui/page-renderer'
-import lambdaDesignerSchema from '@/config/pages/lambda-designer.json'
-import { PageSchema } from '@/types/json-ui'
-
-export function JSONLambdaDesigner() {
-  return (
-    <PageRenderer schema={lambdaDesignerSchema as PageSchema} />
-  )
-}
